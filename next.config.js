88127cdd554e6625cb25a5d555daed01c2c81c6a
Sumit <sumitvermacr7@gmail.com>
2024-04-14 00:10:05 +0530

    path update

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
   },
   images: {
     unoptimized:true // Add the domains for your image sources
-  }, // Set the assetPrefix for GitHub Pages deployment
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/', // Set the assetPrefix for GitHub Pages deployment
 };
